9f3ceddacc746c405356801974186c7ed3af3606
tkak5 <cryptofran1@gmail.com>
2025-01-07 23:00:42 +0900

    reportアップロードapi作成途中

--- front/app/lib/api/reportApi.ts
+++ front/app/lib/api/reportApi.ts
@@ -65,3 +65,83 @@ export const getMonthlyReports = async (
 	const data = await response.json();
 	return data.reports;
 };
+
+export const getAnnualReports = async (): Promise<Report[] | number> => {
+	const user = JSON.parse(localStorage.getItem("user") || "null") as User;
+	const response = await hono.api.reports["annual-reports"].$post(
+		{
+		},
+		{
+			headers: {
+				"Session-Id": user?.sessionId || "",
+			},
+		},
+	);
+
+	if (response.status === 401) {
+		localStorage.removeItem("user");
+		return 401;
+	}
+
+	if (response.status === 500) {
+		console.error("サーバーエラー");
+		return 500;
+	}
+
+	const data = await response.json();
+	return data.reports;
+};
+
+export const createReports = async (
+	year: number,
+	month: number,
+	day: number,
+	title: string,
+	type: "daily" | "monthly" | "annual"
+): Promise<Report[] | number> => {
+	const user = JSON.parse(localStorage.getItem("user") || "null") as User;
+	const response = await hono.api.reports.index.$post(
+		{
+			json: {
+				year,
+				month,
+				day,
+				title,
+				uploaderId: user?.id || "",
+				type,
+			},
+		},
+		{
+			headers: {
+				"Session-Id": user?.sessionId || "",
+			},
+		},
+	);
+
+	if (response.status === 422) {
+		console.error("アップロード失敗");
+		return 422;
+	}
+
+
+	if (response.status === 401) {
+		localStorage.removeItem("user");
+		return 401;
+	}
+
+	if (response.status === 500) {
+		console.error("サーバーエラー");
+		return 500;
+	}
+
+	if (response.status === 201) {
+		localStorage.removeItem("user");
+		return 201;
+	}
+
+	const data = await response.json();
+	return data.reports;
+};
+
+
+
