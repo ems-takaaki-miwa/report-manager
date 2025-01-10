197abfd37954b1ed4ce1670c00337a6b92e7edea
ems-takaaki-miwa <miwa.takaaki@ems-inc.jp>
2025-01-10 09:46:31 +0900

    corsにオリジンを追加

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,19 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
-import reportApi from "./apis/reportApi";
 import authApi from "./apis/authApi";
-import { Bindings } from "./bindings";
+import reportApi from "./apis/reportApi";
+import type { Bindings } from "./bindings";
 
 const middleware = new Hono<{ Bindings: Bindings }>()
 	.use("*", prettyJSON())
 	.use(
 		"*",
 		cors({
-			origin: ["http://localhost:5173", "https://front-7mi.pages.dev"], // ここを変更することで、許可するオリジンを変更できる
+			origin: [
+				"http://localhost:5173",
+				"https://report-manager.pages.dev/login",
+			], // ここを変更することで、許可するオリジンを変更できる
 		}),
 	);
 
